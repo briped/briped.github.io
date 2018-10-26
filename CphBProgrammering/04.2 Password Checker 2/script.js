200e4c4bd0f426e9e789efcabcfc9ad1ca85b92f
briped <briped@live.com>
2018-10-27 00:40:33 +0200

    svar

--- CphBProgrammering/04.2 Password Checker 2/script.js
+++ CphBProgrammering/04.2 Password Checker 2/script.js
@@ -1,1 +1,6 @@
-//TODO omskriv passwordcheckeren fra uge 2 så den skriver til DOM'en i stedet for en alert
+//DONE omskriv passwordcheckeren fra uge 2 så den skriver til DOM'en i stedet for en alert
+// Det var allerede det jeg gjorde. Jeg havde bare droppet script.js.
+
+function doSubmit() {
+    document.getElementById('output').innerHTML = document.getElementById('password').value.length;
+}
