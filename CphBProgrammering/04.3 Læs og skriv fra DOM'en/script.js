7bc17c5a87ad32bdb2c445ab50a0107e91c3b4e2
briped <briped@live.com>
2018-10-27 00:46:06 +0200

    svar

--- CphBProgrammering/04.3 Læs og skriv fra DOM'en/script.js
+++ CphBProgrammering/04.3 Læs og skriv fra DOM'en/script.js
@@ -1,1 +1,6 @@
-//TODO: læs teksten "Læs denne tekst" og indsæt den hvor der står "Og overskriv denne tekst"
+//DONE: læs teksten "Læs denne tekst" og indsæt den hvor der står "Og overskriv denne tekst"
+let elInput = document.getElementById('input');
+let elOutput = document.getElementById('output');
+console.log(`Original indhold af input: ${elInput.innerHTML}`);
+console.log(`Original indhold af output: ${elOutput.innerHTML}`);
+elOutput.innerHTML = elInput.innerHTML;
