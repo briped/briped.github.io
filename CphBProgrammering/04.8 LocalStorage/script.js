aa89004410b7b88200b0932359b00179a4bfb315
briped <briped@live.com>
2018-10-27 17:33:07 +0200

    refactoring and comments and learning

--- CphBProgrammering/04.8 LocalStorage/script.js
+++ CphBProgrammering/04.8 LocalStorage/script.js
@@ -11,9 +11,9 @@ localStorage.getItem('state');
 
 //TODO: Implementere localStorage (skal sætte mig ind i hvordan det virker).let loginState = false;
 // https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem
-
-let loggedInAs = '';
-let passwordLength = 0;
+if (!localStorage.getItem('loginState')) {
+    populateStorage(false, '', 0);
+}
 
 let validLogins = [];
 validLogins.push(['administrator', 'Administrator']);
@@ -32,31 +32,44 @@ elBody.onload = updateGUI;
 elLogin.onclick = validateLogin;
 elLogout.onclick = validateLogin;
 
+function populateStorage(loginState, loggedInAs, passwordLength) {
+    console.log(`function populateStorage(loginState:${loginState}, loggedInAs:${loggedInAs}, passwordLength:${passwordLength})`); // Debugging
+    localStorage.setItem('loginState', loginState);
+    localStorage.setItem('loggedInAs', loggedInAs);
+    localStorage.setItem('passwordLength', passwordLength);
+    console.log(localStorage); // Debugging
+}
+
 function validateLogin() {
+    console.log(`function validateLogin()`); // Debugging
     for (let l = 0; l < validLogins.length; l++) {
         if (elUsername.value == validLogins[l][0] && elPassword.value == validLogins[l][1]) {
-            loginState = true;
-            loggedInAs = elUsername.value;
-            passwordLength = elPassword.value.length;
+            populateStorage(true, elUsername.value, elPassword.value.length);
             elUsername.value = '';
             elPassword.value = '';
             break;
         }
         else {
-            loginState = false;
+            populateStorage(false, '', 0);
         }
     }
     updateGUI();
 }
 
 function updateGUI() {
-    if (loginState === true) {
-        document.getElementById('loggedInAs').innerHTML = loggedInAs;
-        document.getElementById('passwordLength').innerHTML = passwordLength;
+    console.log(`function updateGUI()`); // Debugging
+    console.log(`localStorage.getItem('loginState'):${localStorage.getItem('loginState')}`); // Debugging
+    if (localStorage.getItem('loginState') === 'true') {
+        // Apparently only strings are stored in localStorage, even though a boolean is stored. Apparently even with
+        // loose comparison (localStorage.getItem('loginState') == true) it wont work. Why?
+        console.log("if (localStorage.getItem('loginState') == true) ..."); // Debugging
+        document.getElementById('loggedInAs').innerHTML = localStorage.getItem('loggedInAs');
+        document.getElementById('passwordLength').innerHTML = localStorage.getItem('passwordLength');
         elLoggedOut.style.display = 'none';
         elLoggedIn.style.display = 'block';
     }
     else {
+        console.log("else ..."); // Debugging
         elLoggedOut.style.display = 'table';
         elLoggedIn.style.display = 'none';
     }
