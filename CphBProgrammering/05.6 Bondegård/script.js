9c2106f66f65661664fc84d333b8d8da38ec11c8
briped <briped@live.com>
2018-10-30 17:27:22 +0100

    Svar

--- CphBProgrammering/05.6 Bondegård/script.js
+++ CphBProgrammering/05.6 Bondegård/script.js
@@ -1,20 +1,19 @@
-
 let animals = []; //No animals in array yet...
 
-function Animal(type, sound){
+function Animal(type, sound) {
     this.type = type;
-    this.makeSound = function(){alert("the " + type + " says: " + sound)}
-
+    this.makeSound = function(){alert(`the ${type} says: ${sound}`)}
 }
 
-//TODO make some more animals and fill them into the array!
+//DONE make some more animals and fill them into the array!
 animals.push(new Animal("cat", "Miuow"));
-
+animals.push(new Animal("dog", "Woof"));
+animals.push(new Animal("cow", "Mooh"));
+animals.push(new Animal("sheep", "Baaah"));
+animals.push(new Animal("donkey", "Heehaaw"));
 
 //Print something interesting to the DOM
-for (let i = 0; i<animals.length;i++) {
+for (let i = 0; i < animals.length; i++) {
     //adds a button with an onclick eventhandler that makes a sound!
-    document.getElementById("animaldiv").innerHTML +=
-        "<BR><button onclick='animals[" + i + "].makeSound()'>" + animals[i].type + "</button> "
-
+    document.getElementById("animaldiv").innerHTML += `<br /><button onclick="animals[${i}].makeSound()">${animals[i].type}</button>`;
 }
