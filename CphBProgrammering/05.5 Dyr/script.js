af1c7bfa2555bb9216d1bafffad2f9626943c48e
briped <briped@live.com>
2018-10-30 16:58:39 +0100

    Svar. Trimmed.

--- CphBProgrammering/05.5 Dyr/script.js
+++ CphBProgrammering/05.5 Dyr/script.js
@@ -1,29 +1,22 @@
-
-
-function Animal(name,makeSoundFunction){
+function Animal(name, makeSoundFunction) {
     this.name = name;
-    this.makeSound = makeSoundFunction
+    this.makeSound = makeSoundFunction;
 }
 
-let dog = new Animal("Fido",function(){
-        document.getElementById("output").innerHTML = "WOOF"}
-    );
+let dog = new Animal("Fido", function() {
+    document.getElementById("output").innerHTML = "WOOF"
+});
 
+let cat = new Animal("Garfield", function() {
+    document.getElementById("output").innerHTML = "MIAOW"
+});
 
-
-let cat = new Animal("Garfield", function(){
-    document.getElementById("output").innerHTML = "MIAOW"}
-    );
-
-
-//TODO Add another animal
-
-
-
-
+//DONE Add another animal
+let fox = new Animal("Tod", function(){
+    document.getElementById("output").innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/jofNR_WkoCE?start=40&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+});
 
 //More clever solution below:
-
 function Animal2(name,sound){
     this.name = name;
     this.sound = sound;
