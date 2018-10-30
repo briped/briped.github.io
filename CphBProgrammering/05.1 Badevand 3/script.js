73eee7083417be0cd448a1554d7ad6de46253ff5
Brian Schmidt Pedersen <briped@live.co>
2018-10-30 02:46:17 +0100

    svar

--- CphBProgrammering/05.1 Badevand 3/script.js
+++ CphBProgrammering/05.1 Badevand 3/script.js
@@ -4,10 +4,10 @@ let badeVand =  [-1, 1, 1, 1, 2, 0, 1 ,-0.1, 0.1, -20, -4, 10];
 let numberOfOnes = 0;
 
 
-//TODO count number of ones:
-
+//DONE count number of ones:
+for (i = 0; i < badeVand.length; i++) {
+    numberOfOnes += (badeVand === 1) ? 1 : 0;
+}
 
 //Output the result to a div
 document.getElementById("output").innerHTML = "Antal 1'ere: " + numberOfOnes;
-
-
