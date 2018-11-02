a60dc170282fbfe9283f3b4156c0ff8f2d994e4a
briped <briped@live.com>
2018-11-02 23:04:29 +0100

    checkForWinner. Horizontal win solved (the easy one).

--- CphBProgrammering/00-Eksamen/script.js
+++ CphBProgrammering/00-Eksamen/script.js
@@ -3,7 +3,7 @@ const rows = 6;
 const columns = 7;
 
 // Global variables
-let gameArray, currentPlayer, browserLanguage, board = document.getElementById('board');
+let gameArray, gameWon, currentPlayer, player1score = 0, player2score = 0, browserLanguage, board = document.getElementById('board');
 
 // Start the game
 resetGame();
@@ -30,14 +30,15 @@ function resetGame() {
             gameArray[r][c] = 0;
         }
     }
+    gameWon = 0;
     currentPlayer = 1;
     detectLanguage();
     document.getElementsByTagName('title')[0].innerHTML = lang[browserLanguage].title;
     document.getElementById('title').innerHTML = lang[browserLanguage].title;
     document.getElementById('currentPlayer').className = `circle${currentPlayer}`;
     document.getElementById('currentPlayer').innerHTML = currentPlayer.toString();
-    document.getElementById('player1').innerHTML = `${lang[browserLanguage].Player} 1: <span>0</span>`;
-    document.getElementById('player2').innerHTML = `${lang[browserLanguage].Player} 2: <span>0</span>`;
+    document.getElementById('player1').innerHTML = `${lang[browserLanguage].Player} 1: <span>${player1score}</span>`;
+    document.getElementById('player2').innerHTML = `${lang[browserLanguage].Player} 2: <span>${player2score}</span>`;
     document.getElementById('resetGame').innerHTML = lang[browserLanguage].newGame;
     drawBoard();
 }
@@ -56,25 +57,52 @@ function insertChecker(e) {
     //console.log(`col: ${col}. row: ${row}.`); // Debugging
 
     if (updateArray(currentPlayer, col)) {
+        gameWon = checkForWinner();
+        if (gameWon !== 0) {
+            player1score = (currentPlayer === 1) ? player1score + 1 : player1score;
+            player2score = (currentPlayer === 2) ? player2score + 1 : player2score;
+            document.getElementById('player1').innerHTML = `${lang[browserLanguage].Player} 1: <span>${player1score}</span>`;
+            document.getElementById('player2').innerHTML = `${lang[browserLanguage].Player} 2: <span>${player2score}</span>`;
+            alert(`Player ${currentPlayer} wins!`);
+        }
         updatePlayer();
+        drawBoard();
     }
-    drawBoard();
-    checkConnect4();
 }
 
-function checkConnect4() {
+function checkForWinner(length = 4) {
+    console.log(`function checkForWinner(length: ${length})...`); // Debugging
     //TODO: Check if there's 4 connected. Vertically or Horizontally or Diagonally.
-    let count1h = 0;
-    let count1v = 0;
-    let count1d = 0;
-    let count2h = 0;
-    let count2v = 0;
-    let count2d = 0;
-    for (let r = 0; r < gameArray.length; r++) {
-        for (let c = 0; c < gameArray[r].length; c++) {
-
+    // Horizontal check seems to work. Vertical check doesn't seem to work. Haven't worked on diagonally yet.
+    let horizontal = 0;
+    let vertical = 0;
+    let diagonalTopDown = 0;
+    let diagonalDownTop = 0;
+    let r, c;
+    console.log(gameArray);
+    for (r = gameArray.length - 1; r > 0; r--) {
+        console.log(`... for (r = gameArray.length(${gameArray.length}) - 1{${gameArray.length - 1}; r(${r}) > 0; r++)...`);
+        if (r === gameArray.length - 1) {
+            vertical = 0;
+        }
+        for (c = 0; c < gameArray[r].length; c++) {
+            console.log(`... for (c = 0; c(${c}) < gameArray[r].length(${gameArray[r].length}); c++)...`);
+            if (c === 0) {
+                horizontal = 0;
+            }
+            horizontal = (gameArray[r][c] === currentPlayer) ? horizontal + 1 : 0;
+            console.log(`horizontal: ${horizontal}`);
+            if (horizontal >= length) {
+                return currentPlayer;
+            }
+        }
+        vertical = (gameArray[r][c] === currentPlayer) ? vertical + 1 : 0;
+        console.log(`vertical: ${vertical}`);
+        if (vertical >= length) {
+            return currentPlayer;
         }
     }
+    return 0;
 }
 
 function drawBoard() {
@@ -95,8 +123,8 @@ function drawBoard() {
 
 function updateArray(player = -1, col = -1) {
     console.log(`function updateArray(player:${player}, col:${col})...`); // Debugging
-    if (col < 0 || col >= columns) {
-        return false; // Supplied column isn't valid.
+    if (col < 0 || col >= columns || gameWon !== 0) {
+        return false; // Supplied column isn't valid, or the game have already been won.
     }
     player = (player >= 0 && player <= 2) ? player : 0;
     for (let r = gameArray.length; r > 0; r--) {
