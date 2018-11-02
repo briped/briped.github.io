4dcba155af0bd3e3fac1e0b707fb578d3f525130
briped <briped@live.com>
2018-11-02 18:10:24 +0100

    Layout Strings updateArray

--- CphBProgrammering/00-Eksamen/script.js
+++ CphBProgrammering/00-Eksamen/script.js
@@ -1,33 +1,61 @@
+// Size of the Connect 4 board
 const rows = 6;
 const columns = 7;
 
-/*
-const radius = 40;
-const offset = 50;
+// Global variables
+let gameArray, currentPlayer, browserLanguage, board = document.getElementById('board');
 
-const boardColor = 'Yellow';
-const playerColor = ['white', 'blue', 'red'];
-*/
+// Start the game
+resetGame();
 
-let c4board = document.getElementById('c4board');
-let currentPlayer = 1;
+function detectLanguage() {
+    console.log(`function detectLanguage()...`); // Debugging
+    browserLanguage = 'en'; // Default language i no other is found...
+    for (let l = 0; l < navigator.languages.length; l++) {
+        for (let key in lang) {
+            if (lang.hasOwnProperty(key) && key === navigator.languages[l]) {
+                browserLanguage = key;
+                break;
+            }
+        }
+    }
+}
 
-let c4array;
-updateArray();
-drawBoard();
+function resetGame() {
+    console.log(`function resetGame()...`); // Debugging
+    gameArray = [];
+    for (let r = 0; r < rows; r++) {
+        gameArray[r] = [];
+        for (let c = 0; c < columns; c++) {
+            gameArray[r][c] = 0;
+        }
+    }
+    currentPlayer = 1;
+    detectLanguage();
+    document.getElementsByTagName('title')[0].innerHTML = lang[browserLanguage].title;
+    document.getElementById('title').innerHTML = lang[browserLanguage].title;
+    document.getElementById('currentPlayer').className = `circle${currentPlayer}`;
+    document.getElementById('currentPlayer').innerHTML = currentPlayer.toString();
+    document.getElementById('player1').innerHTML = `${lang[browserLanguage].Player} 1: <span>0</span>`;
+    document.getElementById('player2').innerHTML = `${lang[browserLanguage].Player} 2: <span>0</span>`;
+    document.getElementById('resetGame').innerHTML = lang[browserLanguage].newGame;
+    drawBoard();
+}
 
 function updatePlayer() {
-    //console.log(`function updatePlayer()...`); // Debugging
+    console.log(`function updatePlayer()...`); // Debugging
     currentPlayer = (currentPlayer === 1) ? 2 : 1;
+    document.getElementById('currentPlayer').className = `circle${currentPlayer}`;
+    document.getElementById('currentPlayer').innerHTML = currentPlayer.toString();
 }
 
 function insertChecker(e) {
-    //console.log(`function insertChecker(e: ${e.id})...`); // Debugging
+    console.log(`function insertChecker(e: ${e.id})...`); // Debugging
     let col = Number(e.id.substr(e.id.indexOf('c', 0) + 1));
-    let row = Number(e.id.substr(e.id.indexOf('r', 0) + 1, e.id.length - e.id.indexOf('c', 0) - 1));
+    //let row = Number(e.id.substr(e.id.indexOf('r', 0) + 1, e.id.length - e.id.indexOf('c', 0) - 1));
     //console.log(`col: ${col}. row: ${row}.`); // Debugging
 
-    if (updateArray(row, col, currentPlayer)) {
+    if (updateArray(currentPlayer, col)) {
         updatePlayer();
     }
     drawBoard();
@@ -36,51 +64,45 @@ function insertChecker(e) {
 
 function checkConnect4() {
     //TODO: Check if there's 4 connected. Vertically or Horizontally or Diagonally.
+    let count1h = 0;
+    let count1v = 0;
+    let count1d = 0;
+    let count2h = 0;
+    let count2v = 0;
+    let count2d = 0;
+    for (let r = 0; r < gameArray.length; r++) {
+        for (let c = 0; c < gameArray[r].length; c++) {
+
+        }
+    }
 }
 
 function drawBoard() {
-    //console.log(`function drawBoard()...`); // Debugging
-    c4board.innerHTML = '';
-    for (let r = 0; r < c4array.length; r++)
-    {
+    console.log(`function drawBoard()...`); // Debugging
+    board.innerHTML = '';
+    for (let r = 0; r < gameArray.length; r++) {
         let rowHTML = '';
         let rowHeader = '';
-        for (let c = 0; c < c4array[r].length; c++)
-        {
-            if (r === 0)
-            {
+        for (let c = 0; c < gameArray[r].length; c++) {
+            if (r === 0) {
                 rowHeader += `<button id="c${c}" class="player${currentPlayer}" onclick="insertChecker(this);">${c + 1}</button>`;
             }
-            rowHTML += `<div id="r${r}c${c}" class="circle${c4array[r][c]}" onclick="insertChecker(this);"></div>`;
+            rowHTML += `<div id="r${r}c${c}" class="circle${gameArray[r][c]}" onclick="insertChecker(this);"></div>`;
         }
-        c4board.innerHTML += (r === 0) ? rowHeader + rowHTML: rowHTML;
+        board.innerHTML += (r === 0) ? rowHeader + rowHTML: rowHTML;
     }
 }
 
-function updateArray(row = -1, col = -1, player = -1) {
-    //TODO: Modify so adding by column, and row will always be highest row index available (if any).
-    console.log(`function updateArray(row: ${row}, col: ${col}, player: ${player})...`); // Debugging
-    let arrayChanged = false;
-    player = (row === -1 && col === -1 && player === -1) ? 0 : player;
-    c4array = ((row === -1 && col === -1 && player === -1) || c4array === undefined) ? [] : c4array;
-    for (let r = 0; r < rows; r++) {
-        if (c4array[r] === undefined) {
-            c4array[r] = [];
-        }
-        for (let c = 0; c < columns; c++) {
-            if (c4array[r][c] === undefined) {
-                c4array[r][c] = 0;
-            }
-            if (row === r && col === c && c4array[r][c] === 0) {
-                c4array[r][c] = player;
-                arrayChanged = true;
-            }
+function updateArray(player = -1, col = -1) {
+    console.log(`function updateArray(player:${player}, col:${col})...`); // Debugging
+    if (col < 0 || col >= columns) {
+        return false; // Supplied column isn't valid.
+    }
+    player = (player >= 0 && player <= 2) ? player : 0;
+    for (let r = gameArray.length; r > 0; r--) {
+        if (gameArray[r-1][col] === 0) {
+            gameArray[r-1][col] = player;
+            return true;
         }
     }
-    return (arrayChanged === true);
-}
-
-function drawCircle(player = 0) {
-    console.log(`function drawCircle(player: ${player})...`); // Debugging
-    c4board.innerHTML += `<span class="0"></span>`;
 }
---
+++ CphBProgrammering/00-Eksamen/strings.js
@@ -0,0 +1,12 @@
+let lang = {
+    en: {
+        title: 'Connect 4',
+        newGame: 'New Game',
+        Player: 'Player'
+    },
+    da: {
+        title: 'Fire på stribe',
+        newGame: 'Nyt spil',
+        Player: 'Spiller'
+    }
+};
