6d5a5488fc664890b8496f41e77450b5d5a1d2cf
briped <briped@live.com>
2024-06-05 22:34:32 +0200

    get and watermark covers

--- PowerShell/Get-WalledPods.ps1
+++ PowerShell/Get-WalledPods.ps1
@@ -1,10 +1,18 @@
-function Get-WalledPods {
-	$Pods = Search-DRPodcast -Type series -Query * -Limit 10000 | Where-Object podcastUrl
-	foreach ($p in $Pods) {
-		$r = Invoke-WebRequest -Uri $p.podcastUrl
-		if ($r.Content -match 'utm_source=thirdparty') {
-			$p | Select-Object -Property title, @{N='slug'; E={$p.slug.Replace("-$($p.productionNumber)", '')}}, id
-		}
+$Manifest = [System.IO.FileInfo](Join-Path -Path $PSScriptRoot -ChildPath 'DRPodcast.psd1')
+Import-Module -Force -Name $Manifest
+$WalledPath = Join-Path -Path $PSScriptRoot -ChildPath 'walled.json'
+$WatermarkPath = Join-Path -Path $PodPath -ChildPath 'assets' -AdditionalChildPath 'icon-recycle-469x454.png'
+function Get-WalledPodcast {
+	$Podcasts = Search-DRPodcast -Type series -Query * -Limit 10000 | Where-Object podcastUrl
+	foreach ($Podcast in $Podcasts) {
+		$Response = Invoke-WebRequest -Uri $Podcast.podcastUrl
+		if ($Response.Content -notmatch 'utm_source=thirdparty') { continue }
+		$CoverOrig = Join-Path -Path $PodPath -ChildPath "$($Podcast.sSlug).original.jpg"
+		$CoverPath = Join-Path -Path $PodPath -ChildPath "$($Podcast.sSlug).jpg"
+		Invoke-WebRequest -Uri $Podcast.imageUri -OutFile $CoverOrig
+		Add-Watermark -ImagePath $CoverOrig -WatermarkPath $WatermarkPath -OutputPath $CoverPath -PositionX 0 -PositionY 10 -Width 180 -Opacity 70
+		Remove-Item -Path $CoverOrig
+		$Podcast | Select-Object -Property title, @{N='slug'; E={$Podcast.slug.Replace("-$($Podcast.productionNumber)", '')}}, id
 	}
 }
-Get-WalledPods | ConvertTo-Json | Out-File -Encoding utf8 -FilePath walled.json
+Get-WalledPodcast | ConvertTo-Json | Out-File -Encoding utf8 -FilePath $WalledPath
