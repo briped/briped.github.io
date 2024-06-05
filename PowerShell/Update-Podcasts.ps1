dae70445c1620a7af8e8836bae292a13a4f45ddf
briped <briped@live.com>
2024-06-05 15:24:40 +0200

    Only walled pods

--- PowerShell/Update-Podcasts.ps1
+++ PowerShell/Update-Podcasts.ps1
@@ -2,11 +2,10 @@ $global:ApiKey = Get-Content -TotalCount 1 -Path (Join-Path -Path $PSScriptRoot 
 $global:RssBase = [uri]'https://xmpl.dk/podcast/'
 $Manifest = [System.IO.FileInfo](Join-Path -Path $PSScriptRoot -ChildPath 'DRPodcast.psd1')
 Import-Module -Force -Name $Manifest
-$Favorites = Get-Content -Raw -Encoding utf8 -Path (Join-Path -Path $PSScriptRoot -ChildPath 'favorites.json') | ConvertFrom-Json
 $Walled = Get-Content -Raw -Encoding utf8 -Path (Join-Path -Path $PSScriptRoot -ChildPath 'walled.json') | ConvertFrom-Json
 $FeedsPath = [System.IO.FileInfo](Join-Path -Path (Get-Item -Path $PSScriptRoot).Parent -ChildPath 'podcast')
 
-$Podcasts = $Favorites + $Walled | 
+$Podcasts = $Walled | 
 	Sort-Object -Unique id | 
 	Sort-Object -Property title | 
 	Get-DRPodcast
