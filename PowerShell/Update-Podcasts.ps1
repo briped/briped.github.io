5ece32e8172390f1ef53694aef52f72bbd692427
briped <briped@live.com>
2024-06-09 22:14:41 +0200

    Only update if changed

--- PowerShell/Update-Podcasts.ps1
+++ PowerShell/Update-Podcasts.ps1
@@ -12,7 +12,12 @@ foreach ($Podcast in $Podcasts) {
 		Write-Verbose -Message "Adding episodes to '$($Podcast.title)'."
 		$Podcast | Add-Member -NotePropertyName episodes -NotePropertyValue $(Get-DREpisode -Id $Podcast.id -Limit 10000)
 	}
-	$Podcast | ConvertTo-Json -Depth 10 | Out-File -Force -Encoding utf8 -FilePath $(Join-Path -Path $PodPath -ChildPath 'data' -AdditionalChildPath "$($Podcast.sSlug).json")
-	$Podcast | New-DRRss | Out-File -Force -Encoding utf8 -FilePath $(Join-Path -Path $PodPath -ChildPath "$($Podcast.sSlug).xml")
+	$JsonPath = Join-Path -Path $PodPath -ChildPath 'data' -AdditionalChildPath "$($Podcast.sSlug).json"
+	$OldJson = Get-Content -Encoding utf8 -Path $JsonPath | ConvertFrom-Json -Depth 10
+	$NewJson = $Podcast | ConvertTo-Json -Depth 10
+	if (!(Compare-Object -ReferenceObject $OldJson -DifferenceObject $Podcast)) {
+		$NewJson | Out-File -Force -Encoding utf8 -FilePath $JsonPath
+		$Podcast | New-DRRss | Out-File -Force -Encoding utf8 -FilePath $(Join-Path -Path $PodPath -ChildPath "$($Podcast.sSlug).xml")
+	}
 }
 $Podcasts | New-DRHtml | Out-File -Force -Encoding utf8 -FilePath $(Join-Path -Path $PodPath -ChildPath "index.html")
